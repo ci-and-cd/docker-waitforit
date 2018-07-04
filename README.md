be8d94d287ffd98c14675168ac4f55f54a7dde11
haolun <haolun@example.com>
2018-07-04 23:53:23 +0800

    update build scripts

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Dockerfile [ci-and-cd/waitforit on Github](https://github.com/ci-and-cd/waitfori
 ## Use this image as a “stage” in multi-stage builds
 
 ```dockerfile
+
 FROM alpine:3.7
-COPY --from=cirepo/waitforit:2.2.0 /usr/bin/waitforit /usr/bin/waitforit
+COPY --from=cirepo/waitforit:2.2.0-archvie /data/root /
+
 ```
--- build.sh
+++ build.sh
@@ -7,18 +7,24 @@ docker-compose version
 
 WORK_DIR=$(pwd)
 
-if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then
-    echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io
-fi
+#if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
-IMAGE_NAME=${IMAGE_PREFIX:-cirepo}/waitforit
+export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
+export IMAGE_NAME=${IMAGE_NAME:-waitforit}
 export IMAGE_TAG=${IMAGE_ARG_WAITFORIT_VERSION:-2.2.0}
 if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
 
-docker-compose build
-docker-compose push
-
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${IMAGE_TAG}" == "${IMAGE_TAG_LATEST}" ]; then
-    docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
-    docker push ${IMAGE_NAME}:latest
+# Build image
+if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
+    docker-compose build image
 fi
+
+## Build dumper image
+docker save ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} > dumper/docker/image.tar
+docker-compose build dumper
+
+## Build archive image
+docker-compose build archive
+
+#docker-compose push image
+#docker-compose push archive
