2e1588702e644e172d5dbaf92dd0ee558281255d
haolun <haolun@example.com>
2018-07-05 00:00:14 +0800

    push images

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker-compose version
 
 WORK_DIR=$(pwd)
 
-#if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
+if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
 export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
 export IMAGE_NAME=${IMAGE_NAME:-waitforit}
@@ -26,5 +26,5 @@ docker-compose build dumper
 ## Build archive image
 docker-compose build archive
 
-#docker-compose push image
-#docker-compose push archive
+docker-compose push image
+docker-compose push archive
